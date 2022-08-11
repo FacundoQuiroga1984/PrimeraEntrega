f440e10b8d0e4307a42a56d846e8d31afb3fdbfa
leandro <leandrotancredi87@gmail.com>
2022-08-10 22:11:13 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const persona1={nombre:"Leandro", altura:"1.77"};
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola {persona1.nombre} tu altura es {persona1.altura}cm.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ButtonComponent />
       </header>
     </div>
   );
+}
+const ButtonComponent = () => {
+  return <button>Hola React Js :)</button>
 }
 
 export default App;
